3173335e62bb30a06e7b916d8f807243fa44a53f
Roman Yevchuk <r.yevchuk@gmail.com>
2020-06-12 16:48:41 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+         <ScrollView style={styles.scrollView}>
+            <Text style={styles.text}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                  minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                  aliquip ex ea commodo consequat. Duis aute irure dolor in
+                  reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                  pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                  culpa qui officia deserunt mollit anim id est laborum.
+              </Text>
+          </ScrollView>
+      </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+        marginTop: Constants.statusBarHeight,
+    },
+    scrollView: {
+        backgroundColor: 'pink',
+        marginHorizontal: 20,
+    },
+    text: {
+        fontSize: 42,
+    },
+});
+
+export default App;
